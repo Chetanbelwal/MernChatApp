4eff1de7520c76f71640d902f013623a3367ed7d
Chetan Belwal <chetan.belwal13@gmail.com>
2025-01-10 02:05:59 +0530

    gteMessage between conversation

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -1,6 +1,15 @@
 import Conversation from "../models/conversationModel.js";
 import Message from "../models/messageModel.js";
 
+// Function: sendMessage
+// Purpose: Handles the sending of a message between two users. If a conversation does not already exist between the sender and receiver, it creates a new one.
+// Steps:
+// 1. Retrieves the sender's ID from the request and the receiver's ID from route parameters.
+// 2. Searches for an existing conversation between the two participants. If none exists, it creates a new conversation.
+// 3. Creates a new message with the sender's ID, receiver's ID, and the message content.
+// 4. Adds the message to the conversation's message array and saves the updated conversation.
+// 5. Responds with a success message or logs errors if any occur.
+
 export const sendMessage = async (req, res) => {
   try {
     const senderId = req.id;
@@ -30,6 +39,25 @@ export const sendMessage = async (req, res) => {
       message: "Message sent successfully",
       success: true,
     });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getMessage = async (req, res) => {
+  try {
+    const senderId = req.id;
+    const receiverId = req.params.id;
+
+    const conversation = await Conversation.findOne({
+      participants: { $all: [senderId, receiverId] },
+    }).populate("messages");
+
+    console.log(conversation);
+
+    // const messagesOnly = conversation.messages.map((msg) => msg.message);
+    // console.log(messagesOnly);
+    // res.status(200).json({ messages: `Get all messages ${messagesOnly}`, success: true });
   } catch (error) {
     console.log(error);
   }
--- server/routes/messageRoute.js
+++ server/routes/messageRoute.js
@@ -1,9 +1,10 @@
 import express from "express";
-import {sendMessage} from "../controllers/messageController.js";
+import {sendMessage, getMessage} from "../controllers/messageController.js";
 import authMiddleware from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.route("/send/:id").post(authMiddleware, sendMessage);
+router.route("/:id").get(authMiddleware, getMessage);
 
 export default router;
