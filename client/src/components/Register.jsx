edcc90478b454c4c4130006ed6587c86fdac43ce
Chetan Belwal <chetan.belwal13@gmail.com>
2025-01-12 23:42:44 +0530

    Login Signup Page designed

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,7 +1,125 @@
 import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Register = () => {
-  return <div>Register</div>;
+  const [user, setUser] = useState({
+    fullName: "",
+    username: "",
+    password: "",
+    confirmPassword: "",
+    gender: "",
+  });
+  // const navigate = useNavigate();
+  const handleCheckbox = (gender) => {
+    setUser({ ...user, gender });
+  };
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+    console.log(user);
+    setUser({
+      fullName: "",
+      username: "",
+      password: "",
+      confirmPassword: "",
+      gender: "",
+    });
+  };
+
+  return (
+    <div className="min-w-96 mx-auto">
+      <div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-10 border border-gray-100">
+        <h1 className="text-3xl font-bold text-center">REGISTER</h1>
+        <form onSubmit={onSubmitHandler} action="">
+          <div>
+            <label className="label p-2">
+              <span className="text-base label-text">Full Name</span>
+            </label>
+            <input
+              value={user.fullName}
+              onChange={(e) => setUser({ ...user, fullName: e.target.value })}
+              className="w-full input input-bordered h-10"
+              type="text"
+              placeholder="Enter Full Name"
+            />
+          </div>
+          <div>
+            <label className="label p-2">
+              <span className="text-base label-text">Username</span>
+            </label>
+            <input
+              value={user.username}
+              onChange={(e) => setUser({ ...user, username: e.target.value })}
+              className="w-full input input-bordered h-10"
+              type="text"
+              placeholder="Enter Username"
+            />
+          </div>
+          <div>
+            <label className="label p-2">
+              <span className="text-base label-text">Password</span>
+            </label>
+            <input
+              value={user.password}
+              onChange={(e) => setUser({ ...user, password: e.target.value })}
+              className="w-full input input-bordered h-10"
+              type="password"
+              placeholder="Enter Password"
+            />
+          </div>
+          <div>
+            <label className="label p-2">
+              <span className="text-base label-text">Confirm Password</span>
+            </label>
+            <input
+              value={user.confirmPassword}
+              onChange={(e) =>
+                setUser({ ...user, confirmPassword: e.target.value })
+              }
+              className="w-full input input-bordered h-10"
+              type="password"
+              placeholder="Enter Confirm Password"
+            />
+          </div>
+          <div className="flex items-center my-4">
+            <div className="flex items-center">
+              <p>Male</p>
+              <input
+                type="checkbox"
+                checked={user.gender === "male"}
+                onChange={() => handleCheckbox("male")}
+                className="checkbox mx-2"
+              />
+            </div>
+            <div className="flex items-center">
+              <p>Female</p>
+              <input
+                type="checkbox"
+                checked={user.gender === "female"}
+                onChange={() => handleCheckbox("female")}
+                className="checkbox mx-2"
+              />
+            </div>
+          </div>
+          <p className="text-center my-2">
+            Already have an account?{" "}
+            <Link className="text-blue-600" to="/login">
+              {" "}
+              LOGIN{" "}
+            </Link>
+          </p>
+          <div>
+            <button
+              type="submit"
+              className="btn btn-block btn-sm mt-2 border border-slate-700"
+            >
+              Register
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
