d5d66afc3ace2d71f619b4947dd5f9d14a6a8207
Chetan Belwal <chetan.belwal13@gmail.com>
2025-01-21 16:22:54 +0530

    store updated to bypass error

--- client/src/store/store.jsx
+++ client/src/store/store.jsx
@@ -1,4 +1,4 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore , getDefaultMiddleware } from "@reduxjs/toolkit";
 import userSlice from "./userSlice";
 import messageSlice from "./messageSlice";
 import socketSlice from "./socketSlice";
@@ -9,6 +9,15 @@ const store = configureStore({
     message: messageSlice.reducer,
     socket: socketSlice.reducer,
   },
+// Was getting Serialized value to be stored on store of socket so I added this middleware to bypass that error
+
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ["socket/setSocket"], // Ignore this action
+        ignoredPaths: ["socket"], // Ignore the state path where the socket is stored
+      },
+    }),
 });
 
 export default store;
