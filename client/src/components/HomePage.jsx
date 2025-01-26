4b9813f6a7d1c3835a4f3db076d54c08665e88b5
Chetan Belwal <chetan.belwal13@gmail.com>
2025-01-27 02:32:19 +0530

    MessageContainer name

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Sidebar from "./Sidebar";
-import MessageContainer from "./MessageContainer"
+import MessageContainer from "./MessageContainer.jsx";
 
 const HomePage = () => {
   return (
