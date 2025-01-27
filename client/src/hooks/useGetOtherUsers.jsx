5f34d67449a2953700b454996e86a876bb7cd1c4
Chetan Belwal <chetan.belwal13@gmail.com>
2025-01-28 00:49:38 +0530

    Getting Logs on API

--- client/src/hooks/useGetOtherUsers.jsx
+++ client/src/hooks/useGetOtherUsers.jsx
@@ -1,25 +1,25 @@
 import { useEffect } from "react";
 import axios from "axios";
 import { setOtherUsers } from "../store/userSlice";
-import { useDispatch } from "react-redux";  
+import { useDispatch } from "react-redux";
 
 const useGetOtherUsers = () => {
   const dispatch = useDispatch();
-  const BASE_URL = process.env.REACT_APP_BASE_URL
+  const BASE_URL = process.env.REACT_APP_BASE_URL;
+
   useEffect(() => {
     const fetchOtherUsers = async () => {
       try {
-        axios.defaults.withCredentials = true;
-        const res = await axios.get(
-          `${BASE_URL}/api/v1/user/getOtherUsers`
-        );
+        axios.defaults.withCredentials = true; // Include cookies in requests
+        const res = await axios.get(`${BASE_URL}/api/v1/user/getOtherUsers`);
 
-        // console.log(res.data.otherUsers);
+        console.log("Response:", res.data); // Debugging logs
         dispatch(setOtherUsers(res.data.otherUsers));
       } catch (error) {
-        console.log(error);
+        console.error("Error Fetching Other Users:", error.response || error);
       }
     };
+
     fetchOtherUsers();
   }, []);
 };
